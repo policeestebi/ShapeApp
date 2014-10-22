dcfea2d8c90cc6de54e7e7a8ef355f0b16af18de
Esteban Ramírez González <policeestebigmail.com>
2014-10-22 15:52:05 -0700

    Add shape class

---
+++ ShapeApp.Entities/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeApp.Entities
+{
+    public struct Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+}
---
+++ ShapeApp.Entities/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeApp.Entities
+{
+    public abstract class Shape
+    {
+
+        #region Constructor
+
+        #endregion
+
+        #region Methods
+
+        public abstract float GetArea();
+
+        public abstract bool IsPointInside(Point point);
+
+        public abstract bool IsValidData(string entry);
+
+        #endregion
+
+        #region Properties
+
+        public string PatternEntry { get; set; }
+
+        public string ShapeName { get; set; }
+        
+        #endregion
+
+        #region Attributes
+        
+        #endregion
+
+    }
+}
