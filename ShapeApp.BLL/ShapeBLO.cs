f46cea203f8586b85cee994482b67538d026105a
Esteban Ramírez González <policeestebigmail.com>
2014-10-24 15:32:52 -0700

    New ShapeBLO class added

--- ShapeApp.BLL/ShapeBLO.cs
+++ ShapeApp.BLL/ShapeBLO.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ShapeApp.DAL.ShapeFactory;
 using ShapeApp.DAL;
+using ShapeApp.DAL.Interfaces;
 
 namespace ShapeApp.BLL
 {
@@ -12,26 +13,57 @@ namespace ShapeApp.BLL
     {
         #region Constructor
 
-        public ShapeBLO(ShapeDAO shape)
+        public ShapeBLO(ShapeDAO shape,IShapeFactory shapeFactory)
         {
             ShapeDAO = shape;
+            ShapeFactory = shapeFactory;
         }
 
         public ShapeBLO()
         {
             ShapeDAO = ShapeDAO.Instance;
+            ShapeFactory = new ShapeFactory();
         }
 
         #endregion
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shapeDefinition"></param>
+        /// <returns></returns>
         public bool AddShape(string shapeDefinition)
         {
-            
+            if(ShapeDAO == null || ShapeFactory == null) return false;
+
+            var shape = ShapeFactory.CreateShape(shapeDefinition);
+
+            if (shape == null) return false;
+
+            return ShapeDAO.Insert(shape);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool DeleteShape(int id)
+        {
+            if (ShapeDAO == null) return false;
 
+            return ShapeDAO.Delete(id);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public ShapeDAO ShapeDAO { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IShapeFactory ShapeFactory { get; set; }
 
     }
 }
